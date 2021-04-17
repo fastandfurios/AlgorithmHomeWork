f5c45af3f24ad7e966d74b9ef2f0a9134f597a4d
dm <semu-dima@yandex.ru>
2021-04-17 09:38:43 +0300

    create new project, create new branch

---
+++ AlgorithmHomeWork/DirWithTask1/LinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2.DirWithTask1
+{
+    public class LinkedList : ILinkedList
+    {
+	    public int GetCount()
+	    {
+		    throw new NotImplementedException();
+	    }
+
+	    public void AddNode(int value)
+	    {
+		    throw new NotImplementedException();
+	    }
+
+	    public void AddNodeAfter(Node node, int value)
+	    {
+		    throw new NotImplementedException();
+	    }
+
+	    public void RemoveNode(int index)
+	    {
+		    throw new NotImplementedException();
+	    }
+
+	    public void RemoveNode(Node node)
+	    {
+		    throw new NotImplementedException();
+	    }
+
+	    public Node FindNode(int searchValue)
+	    {
+		    throw new NotImplementedException();
+	    }
+    }
+}

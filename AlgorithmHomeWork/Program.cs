272cccda4db65baa2d956739e6b68aa753a29372
dm <semu-dima@yandex.ru>
2021-04-22 23:20:24 +0300

    add tests performance, add img summary

--- AlgorithmHomeWork/Program.cs
+++ AlgorithmHomeWork/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using Lesson4.TestsPerformanceHashSetAndArray;
+using BenchmarkDotNet.Running;
 
-namespace AlgorithmHomeWork
+
+namespace Lesson4
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			
+			BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
 		}
 	}
 }

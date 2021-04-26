e5a5f2223f4433071f5d254c434db2ef777f9b57
dm <semu-dima@yandex.ru>
2021-04-26 20:15:31 +0300

    add's new classes

---
+++ AlgorithmHomeWork/BinaryTree/ITree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4.BinaryTree
+{
+    public interface ITree
+    {
+	    TreeNode GetRoot();
+	    void AddItem(int value);
+	    void RemoveItem(int value);
+	    TreeNode GetNodeByValue(int value);
+	    void PrintTree();
+    }
+}

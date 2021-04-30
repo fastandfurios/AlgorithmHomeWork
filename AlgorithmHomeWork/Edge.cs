f7defa0479401eb527dbe3c130c4e64f469bace4
dm <semu-dima@yandex.ru>
2021-04-30 22:12:14 +0300

    create new project

---
+++ AlgorithmHomeWork/Edge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+    public class Edge
+    {
+	    public int Weight { get; set; }
+	    public Node Node { get; set; }
+    }
+}

6d34af289c6369c3041241b9a947d8ba9e8f3f9e
dm <semu-dima@yandex.ru>
2021-05-02 23:24:14 +0300

    change class graf, add unit tests

--- AlgorithmHomeWork/Program.cs
+++ AlgorithmHomeWork/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace AlgorithmHomeWork
+namespace Lesson6
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var graf = new Graf(8);
+			graf.AddEdge(0, 1);
+			graf.AddEdge(0, 2);
+			graf.AddEdge(0, 7);
+			graf.AddEdge(1, 4);
+			graf.AddEdge(2, 3);
+			graf.AddEdge(2, 4);
+			graf.AddEdge(3, 5);
+			graf.AddEdge(3, 6);
+			graf.AddEdge(6, 7);
+
+			Console.WriteLine("BFS: ");
+			var result = graf.BreadthFirstSearch(0, 5);
+			Console.WriteLine();
+			Console.WriteLine("DFS:");
+			result = graf.DepthFirstSearch(0, 5);
 		}
 	}
 }
